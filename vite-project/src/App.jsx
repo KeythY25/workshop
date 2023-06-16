c5cd7e465c951c52707f6521aa09f8cae2d8d406
Keyth Ybanez <ybanezkeyth25@gmail.com>
2023-06-16 05:53:17 -0700

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useState } from 'react'
+import { puppyList } from './data'
+import './App.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList);
+  
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
+}
+
+export default App
